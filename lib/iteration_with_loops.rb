8d46c989c2a743ea07dfe24a7df487512660d25f
Felix Rodriguez <frod25@gmail.com>
2020-04-28 20:37:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,7 @@
 def find_even_values(src)
   src.length.times { |idx|
     src[idx].length.times {|idx2|
-      puts src[idx][idx2].even? 
-
+      if src[idx][idx2].even? : p src[idx][idx2] end
     }
   }
 end
